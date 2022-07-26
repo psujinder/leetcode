35de3781828ea797c43d97c766349453d626a673
Sujinder Pothula <psujinder@hotmail.com>
2022-07-26 11:21:29 -0400

    updated solution for 338

--- LeetCode.Problems/338_CountingBits.cs
+++ LeetCode.Problems/338_CountingBits.cs
@@ -5,17 +5,14 @@ public class CountingBits{
     public int[] CountBits(int n){
 
         int[] ans = new int[n+1];
-        ans[0] = 0;
 
-        for (int i= 1; i <= n; i++){
+        for (int i= 0; i <= n; i++){
 
-            if( i % 2 == 0){
-                ans[i] = ans[i/2];
-            }else{
-                ans[i] = ans[i/2] +1;
-            }
+            ans[i] = (i%2 == 0) ? ans[i] = ans[i/2] :  ans[i] = ans[i/2] +1;
+            
         }
 
+        return ans;
     }
 
 }
