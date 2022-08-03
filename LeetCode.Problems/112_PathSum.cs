021150120ece74270e8eaa935ddee17927c5c155
Sujinder Pothula <psujinder@hotmail.com>
2022-08-03 15:48:36 -0400

    solved leetcode 112

---
+++ LeetCode.Problems/112_PathSum.cs
@@ -0,0 +1,25 @@
+namespace LeetCode.Problems;
+
+using LeetCode.Problems.Utils;
+
+public class PathSum {
+
+    public bool HasPathSum(TreeNode root, int targetSum){
+
+       if(root == null){
+            return false;
+       }
+
+       targetSum -= root.val;
+
+       if(root.left == null && root.right == null){
+            return (targetSum == 0);
+       }
+
+       return HasPathSum(root.left, targetSum) || HasPathSum(root.right, targetSum);
+        
+    
+
+    }
+
+}
