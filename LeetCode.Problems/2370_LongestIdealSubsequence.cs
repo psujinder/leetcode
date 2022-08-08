1ec55a968fadfd0203ed6007086550fc76b0e50e
Sujinder Pothula <psujinder@hotmail.com>
2022-08-08 02:55:32 -0700

    commit

--- LeetCode.Problems/2370_LongestIdealSubsequence.cs
+++ LeetCode.Problems/2370_LongestIdealSubsequence.cs
@@ -6,18 +6,22 @@ public class LongestIdealSubsequence{
 
     public int LongestIdealString(string s, int k){
 
+
+        //create an array reprsenting the alphabets
         int[] dp = new int[26];
+        //initilize the array to 0 indicating the the string has not be interated.
         Array.Fill(dp,0);
 
-        for(int i = 0; i< s.Length; i++){
-
+        //iterate through the string
+        for(int i=0; i<s.Length; i++){
             int ch = s[i] - 'a';
 
-            for(int j = ch; j >=0 && j >= ch-k; j--){
-                dp[ch] = Math.Max(dp[ch], dp[j]+1);
+            //the element has range from ch-k to ch+k
+            for(int j = ch; j > 0 && j <ch-k; j--){
+                dp[ch] = Math.Max(dp[ch],dp[j]+1);
             }
 
-            for(int j=ch+1; j <26 && j <= ch+k; j++){
+            for(int j =ch+1; j<26 && j<ch+k; j++){
                 dp[ch] = Math.Max(dp[ch], dp[j]+1);
             }
         }
