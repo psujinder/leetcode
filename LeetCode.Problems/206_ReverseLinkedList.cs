0a6000f28b01780c7d8e61a5e58ba7b32f7fa851
Sujinder Pothula <psujinder@hotmail.com>
2022-09-10 22:40:36 -0400

    solved leetcode 206

---
+++ LeetCode.Problems/206_ReverseLinkedList.cs
@@ -0,0 +1,22 @@
+namespace LeetCode.Problems;
+
+using LeetCode.Problems.Utils;
+
+public class ReverseLinkedList{
+
+    public ListNode ReverseList(ListNode head) {
+
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while(curr != null){
+            ListNode tmp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = tmp;
+        }
+
+        return prev;
+    }
+
+}
