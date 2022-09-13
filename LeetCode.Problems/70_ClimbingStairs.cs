8acaea78bc689d8545c3bbeff35998c23f9fb2bd
Sujinder Pothula <psujinder@hotmail.com>
2022-09-12 21:33:06 -0400

    solved leetcode 70

---
+++ LeetCode.Problems/70_ClimbingStairs.cs
@@ -0,0 +1,23 @@
+namespace LeetCode.Problems;
+
+public class ClimbingStairs{
+
+    public int ClimbingStairsCount(int n){
+
+        if(n == 1){
+            return 1;
+        }
+
+        int first = 1;
+        int second = 2;
+
+        for(int i=3; i<= n; i++){
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+
+        return second;
+    }
+
+}
