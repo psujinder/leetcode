666187ee8fe30e2234ffd127f096ff4f4ffd38e3
Sujinder Pothula <psujinder@hotmail.com>
2022-09-13 14:17:26 -0400

    solved leetcode 278

---
+++ LeetCode.Problems/278_FirstBadVersion.cs
@@ -0,0 +1,22 @@
+namespace LeetCode.Problems;
+
+public class FirstBadVersion{
+
+    public int FirstBadVersionNumber(int n){
+
+        int l = 1;
+        int r = n;
+
+        while(l < r){
+            int mid = (l-r)/2;
+            if (isBadVersion(mid)){
+                r = mid;
+            }else{
+                l = mid +1;
+            }
+        }
+
+        return l;
+    }
+
+}
