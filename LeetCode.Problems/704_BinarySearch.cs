3e6b437dd4e3c4be2bb60d692986d0d4fae54eda
Sujinder Pothula <psujinder@hotmail.com>
2022-09-13 12:53:41 -0400

    solved leetcode 704

---
+++ LeetCode.Problems/704_BinarySearch.cs
@@ -0,0 +1,26 @@
+namespace LeetCode.Problems;
+
+public class BinarySearch{
+    public int Search(int[] nums, int target){
+
+        int left = 0;
+        int right = nums.Length - 1;
+        int idx = 0;
+
+        while (left <= right){
+            idx = left + right /2;
+            if(nums[idx] == target){
+                return idx;
+            }  
+
+            if (nums[idx] < target){
+            left = idx + 1;
+            }
+
+            if (nums[idx] > target){
+            right = idx - 1;
+            }
+        }
+        return -1;
+    }
+}
