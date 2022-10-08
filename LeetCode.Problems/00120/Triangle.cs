c56ecabb98e8ce918ba64f46f1da1cbfc0088f8a
Sujinder Pothula <psujinder@hotmail.com>
2022-10-08 19:16:48 +0400

    solved leetcode 120

--- LeetCode.Problems/00120/Triangle.cs
+++ LeetCode.Problems/00120/Triangle.cs
@@ -4,25 +4,27 @@ public class Triangle{
 
    public int MinimumTotal(IList<IList<int>> triangle) {
 
-        int result;
-        int minIdx = 0;
-        result = triangle[0][0];
+      if(triangle.Count == 0){
+         return 0;
+      }
 
-        if(triangle.Count == 1){
-            return result;
-        }
+      if(triangle.Count == 1){
+         return triangle[0][0];
+      }
+ 
+      for(int i = 1; i< triangle.Count; i++){
+         for(int j = 0; j < triangle[i].Count; j++){
+            if(j-1 < 0){
+               triangle[i][j] = triangle[i-1][j]+triangle[i][j];
+            } else if (j >= triangle[i-1].Count){
+               triangle[i][j] = triangle[i-1][j-1]+triangle[i][j];
+            }else{
+               triangle[i][j] = Math.Min(triangle[i-1][j-1]+triangle[i][j], triangle[i-1][j]+triangle[i][j]);
+            }
+         }
+      }
 
-        for(int i=1; i < triangle.Count; i++){
-             
-             if(triangle[i][minIdx] < triangle[i][minIdx+1]){
-                result = result + triangle[i][minIdx];
-             }else{
-                result = result + triangle[i][minIdx+1];
-                minIdx++;
-             }
-        }
-
-        return result;
+      return triangle.Last().Min();
 
    }
 
