57d5d85a599115d80874936d07dda12b3a7f27c3
psujinder <39632145+psujinder@users.noreply.github.com>
2022-10-29 07:19:53 -0400

    solved leetcode 1710

--- LeetCode.Problems/01710/MaxUnitsOnTruck.cs
+++ LeetCode.Problems/01710/MaxUnitsOnTruck.cs
@@ -9,8 +9,9 @@ public class MaxUnitsOnTruck{
         Array.Sort(boxTypes, (a,b)=> b[1].CompareTo(a[1]));
 
         for(int i=0; i< boxTypes.Length; i++){
+            int boxCount = 0;
 
-            int boxCount = Math.Min(remainingBoxes, boxTypes[i][0]);
+            boxCount = (boxTypes[i][0] < remainingBoxes) ? boxTypes[i][0]:remainingBoxes;
             ans += boxCount * boxTypes[i][1];
             remainingBoxes -= boxCount;
 
