c91cec2b025a8e9952d0b2b4275a609230166a94
psujinder <39632145+psujinder@users.noreply.github.com>
2023-02-28 16:51:27 -0500

    solved leetcode 371

---
+++ LeetCode.Problems/00371/sumOfTwoInt.cs
@@ -0,0 +1,16 @@
+namespace LeetCode.Problems;
+
+public class SumOfTwoIntegers{
+
+    public int GetSum(int a, int b) {
+        
+        while (b!=0){
+            int carry = a&b;
+            a = a^b;
+            b = carry << 1;
+        }
+
+        return a;
+    }
+
+}
