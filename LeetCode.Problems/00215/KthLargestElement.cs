8866b65b502af27afc52b28d7d36f7643c6af176
psujinder <39632145+psujinder@users.noreply.github.com>
2023-03-04 23:24:38 -0500

    solved leetcode 215

--- LeetCode.Problems/00215/KthLargestElement.cs
+++ LeetCode.Problems/00215/KthLargestElement.cs
@@ -2,12 +2,53 @@ namespace LeetCode.Problems;
 using System.Collections.Generic;
 
 public class KthLargestElement{
-    /*public int FindKthLargest(int[] nums, int k) {
+
+    public int FindKthLargest(int[] nums, int k) {
         
-        PriorityQueue<int, int> queue = new PriorityQueue<int, int>();
+        int n = nums.Length;
 
-        
+        if (n < k){
+            return -1;
+        }
 
-    }*/
+        int l = 0;
+        int r = n-1;
 
+        while(true){
+
+            int pivotIndex = Partition(nums, l, r);
+            int rank = n - pivotIndex;
+
+            if(rank == k){
+                return nums[k];
+            }else if (rank >k){
+                l = pivotIndex+1;
+            }else{
+                r = pivotIndex -1;
+            }
+        }
+
+    }
+
+    private int Partition(int[] nums, int l, int r){
+
+        int pivotIndex = l;
+        int pivotIndexValue = nums[r];
+
+        for(int i=l; i<=r; i++){
+            if(nums[i]< pivotIndexValue){
+                Swap(nums, i, pivotIndex);
+                pivotIndex++;
+            }
+        }
+
+        Swap(nums, pivotIndex, r);
+        return pivotIndex;
+    }
+
+    private void Swap(int[] nums, int i, int j){
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
 }
