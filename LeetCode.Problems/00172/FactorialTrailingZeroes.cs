af6c77c5621740e778c6c11fcc0d87cb8951579f
psujinder <39632145+psujinder@users.noreply.github.com>
2023-03-05 23:11:06 -0500

    solved leetcode 172

---
+++ LeetCode.Problems/00172/FactorialTrailingZeroes.cs
@@ -0,0 +1,17 @@
+namespace LeetCode.Problems;
+
+public class FactorialTrailingZeroes{
+
+    public int TrailingZeroes(int n) {
+
+        int count = 0;
+
+        while (n > 0){
+            count += n/5;
+            n /=5;
+        }
+
+        return count;
+    }
+
+}
